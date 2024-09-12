6fe3c310c3448bc0e4046ff6086bc6adf8e5afe0
gazdagl <127188053+gazdagl@users.noreply.github.com>
2024-09-12 20:31:09 +0200

    started to define pins

---
+++ szabina_orgona.ino
@@ -0,0 +1,17 @@
+#define keyboard_data_bus_0 0
+#define keyboard_data_bus_1 1
+#define keyboard_data_bus_2 2
+#define keyboard_data_bus_3 3
+#define keyboard_data_bus_4 4
+#define keyboard_data_bus_5 5
+#define keyboard_data_bus_6 6
+#define keyboard_data_bus_7 7
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
